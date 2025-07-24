7be574fac909034511813b4d9bfb36ab6635dd0d
Bhupendra Kumar <149204876+bhupendragangwar@users.noreply.github.com>
2025-07-24 13:50:36 +0000

    component import and render

--- 01-ticketGen-ReactApp/src/App.jsx
+++ 01-ticketGen-ReactApp/src/App.jsx
@@ -1,10 +1,28 @@
 import "./App.css";
+import React, { useState } from "react";
+
+import TicketForm from "./components/TicketForm";
+import TicketDisplay from "./components/TicketDisplay";
 
 function App() {
+  const [ticketData, setTicketData] = useState(null);
   return (
-    <>
-      <p>Ticket Gen App</p>
-    </>
+    // <div>
+    //   {!ticketData ? (
+    //     <TicketForm onGenerate={setTicketData} />
+    //   ) : (
+    //     <TicketDisplay {...ticketData} />
+    //   )}
+    // </div>
+
+    <div className="App">
+      {!ticketData ? (
+        <TicketForm onGenerate={setTicketData} />
+      ) : (
+        <TicketDisplay {...ticketData} />
+      )}
+    </div>
+
   );
 }
 
