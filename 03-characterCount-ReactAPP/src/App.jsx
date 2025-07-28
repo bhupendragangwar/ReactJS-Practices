84e3d5c68fee72cccd6ee833a510afadf7185d5b
Bhupendra Kumar <149204876+bhupendragangwar@users.noreply.github.com>
2025-07-28 05:02:19 +0000

    Setup global Context for theme toggle Dark to light as wise varsa

--- 03-characterCount-ReactAPP/src/App.jsx
+++ 03-characterCount-ReactAPP/src/App.jsx
@@ -1,11 +1,42 @@
 import './App.css'
+// import Home from './components/Home'
+
+import { ThemeProvider } from './contextProvider/ThemeContext'
+
+// import useTheme from './components/ThemeToggle'
+import Navbar from './components/headers/Navbar'
 
 function App() {
 
+  // const { theme, toggleTheme } = useTheme('light')
+
   return (
-    <>
-    <h2>Character Cout Project</h2>
-    </>
+    // <div
+    // style = {{
+    //   height: "100vh",
+    //     transition: "0.3s ease-in",
+    //     colorScheme: "light dark",
+    //   color : theme === 'light' ? 'white' : '#242424',
+    //   backgroundColor: theme === 'light' ? '#242424' : 'white'
+    // }}
+    // >
+    //   <Navbar />
+    //   <button onClick={toggleTheme}
+    //   style={{
+    //     background: theme === "light" ? "white" : "black",
+    //       color: theme === "light" ? "black" : "white",
+    //   }}
+    //   >
+    //     toggleTheme
+    //   </button>
+    //   <h2>Character Cout Project</h2>
+
+    //   <Home />
+    // </div>
+
+    <ThemeProvider>
+      <Navbar />
+    </ThemeProvider>
   )
 }
 
