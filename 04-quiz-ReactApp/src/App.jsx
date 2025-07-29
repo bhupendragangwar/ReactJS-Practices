b77e3a8b5350e979ecb166c5ad1060034417bb18
Bhupendra Kumar <149204876+bhupendragangwar@users.noreply.github.com>
2025-07-29 06:05:48 +0000

    design ready

--- 04-quiz-ReactApp/src/App.jsx
+++ 04-quiz-ReactApp/src/App.jsx
@@ -1,10 +1,20 @@
 import './App.css'
 
+import Navbar from './component/Navbar'
+import { ThemeProvider } from './Provider/ThemeProvider'
+
+import WelComeScreen from './component/WelcomeQuizes'
+// import ScoreScreen from './component/ScoreScreen'
+
 function App() {
 
   return (
     <>
-    <h1>React Quiz App</h1>
+      <ThemeProvider>
+        <Navbar />
+        {/* <ScoreScreen /> */}
+        <WelComeScreen />
+      </ThemeProvider>
     </>
   )
 }
