94145c6a57c226f588bb3a3c6c4a0e911f50d0d7
Bhupendra Kumar <149204876+bhupendragangwar@users.noreply.github.com>
2025-07-29 06:00:44 +0000

    score screen

---
+++ 04-quiz-ReactApp/src/component/ScoreScreen.jsx
@@ -0,0 +1,20 @@
+export default function ScoreScreen() {
+    return (
+        <div>
+            <div>
+                <h2>Quiz Completed <br />
+                    <strong>
+                        Your Scored...
+                    </strong>
+                </h2>
+            </div>
+            <div>
+                <hp>Your Accessbility</hp>
+                <h1>Score Here</h1>
+                <p>Out of 10</p>
+            </div>
+
+            <button className="btn">Play Again</button>
+        </div>
+    )
+}
