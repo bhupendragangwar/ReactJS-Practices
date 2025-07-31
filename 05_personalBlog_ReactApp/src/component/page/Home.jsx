3616a56373ba7dd572289184bb81ff265f1367d8
Bhupendra Kumar <149204876+bhupendragangwar@users.noreply.github.com>
2025-07-31 07:26:45 +0000

    Home page done - half

--- 05_personalBlog_ReactApp/src/component/page/Home.jsx
+++ 05_personalBlog_ReactApp/src/component/page/Home.jsx
@@ -1,7 +1,21 @@
-export default function Home(){
-    return(
-        <div>
-            This is My Home Page
+export default function Home() {
+    return (
+        <div className="my-2 mx-4 p-4 border-l border-r border-b border-grey-400 h-screen">
+            <div className="border-b ">
+                <div>
+                    <h1 className="text-2xl font-bold inline-block border-b-5 border-blue-400">Hi, I'm Bhupendra Gangwar👋</h1>
+                    <p className="my-5">I'm on a journey to become a fFrontent and backend developer & MERN Stack Develope. I have building little projects, trying out new coding techniquies, and sharing what I learn along the way. When I'm not at my desk, You'll find me reading, hiking through the mountains, or challenging myself on rock-climbing walls.</p>
+                    <p className="my-3">I started this blog to document my progress, keep myself accountable, and hopefully inspire anyone else who's learing to code.</p>
+                    <p>Welcome to my corner of the internet, and thanks for stopping by!</p>
+                </div>
+
+                <div>
+                <button className="btn">Github</button>
+                <button className="btn">Linked</button>
+                <button className="btn">Instagram</button>
+                <button className="btn">Discord</button>
+                </div>
+            </div>
         </div>
     )
 }
